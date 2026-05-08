48f7ae1c614e203c7bdd0faaeb0d9fd07057d519
Khansumber Altankhuyag <hanaaa@Khansumbers-MacBook-Pro.local>
2026-05-08 14:27:54 -0700

    Fix: bind to Render's PORT env var

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,7 @@
 import multiprocessing
+import os
 
-bind = "0.0.0.0:10000"
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 accesslog = "-"
